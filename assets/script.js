e4430904139df3952ba2e6d17ddd2e8d7522faab
Minami Itsukaichi <mitukaichi@gmail.com>
2023-09-28 21:14:17 -0700

    add countdown function and time over screen placeholder

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,118 @@
+//  counter
+var i = 0;
+var c = 0;
+var w = 0;
+
+// Create a timer function that starts at the start button click
+
+var secondsLeft = 60;
+var remainingTime = document.querySelector("#remaining_time");
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    remainingTime.textContent = secondsLeft;
+    if(secondsLeft === 0) {
+     clearInterval(timerInterval);
+     timeOver();
+    }
+  }, 1000);
+};
+
+
+// Start the quiz at the click of the start button
+var startButton = document.querySelector("button");
+startButton.addEventListener("click",function(){
+    i = 0;
+    newQuiz();
+    setTime();
+});
+
+// Switch to time out screen when the remaining second is 0 
+
+function timeOver() {
+    mainPageHeader.textContent = "Time is over!"
+    mainContainer.textContent = "You made " + c + " correct answers and " + w + " wrong answers."
+};
+
+// Display qiuz contents on the page
+// Create unordered list element
+var mainPageHeader = document.querySelector("h2");
+var mainContainer = document.getElementById("main_content");
+
+var list = document.createElement("ul");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+// Show new quiz
+function newQuiz() {
+    mainPageHeader.textContent = quizList[i].question;
+    li1.textContent = quizList[i].options[0];
+    li2.textContent = quizList[i].options[1];
+    li3.textContent = quizList[i].options[2];
+    li4.textContent = quizList[i].options[3];
+    mainContainer.appendChild(list);
+    list.appendChild(li1);
+    list.appendChild(li2);
+    list.appendChild(li3);
+    list.appendChild(li4);
+};
+
+// Add answer check and style change when one of the optisons are selected
+var bottomText = document.getElementById("bottom_content");
+
+function answerSelect(listNum, rightAnswerLocation) {
+    if (quizList[i].rightAnswer === rightAnswerLocation) {
+        listNum.setAttribute("class", "correct");
+        c++;
+        bottomText.textContent = "It's correct! You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
+    } else {
+        listNum.setAttribute("class", "wrong");
+        w++;
+        secondsLeft = secondsLeft - 9;
+        bottomText.textContent = "It's wrong. You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
+    };
+    // Prevent from users to click again
+    list.setAttribute("style","pointer-events: none;");
+    // Wait for one second and show new quiz
+    setTimeout(function(){
+        if (i < quizList.length - 1){
+            i++;
+            newQuiz();
+            listNum.setAttribute("class", "");
+            bottomText.textContent = "";
+            list.setAttribute("style","pointer-events: auto;");
+        } else {
+            // Ask user to wait when there is no more questions 
+            mainPageHeader.textContent = "";
+            mainContainer.textContent = "There is no more quiz, sorry! Please wait until the remaining time becomes 0.";
+            bottomText.textContent = "";
+        }
+
+    },1000)
+};
+
+// Perform the answer check function at the click of each item
+li1.addEventListener("click",function(event){
+    answerSelect(li1,0);
+});
+
+li2.addEventListener("click",function(event){
+    answerSelect(li2,1);
+});
+
+li3.addEventListener("click",function(event){
+    answerSelect(li3,2);
+});
+
+li4.addEventListener("click",function(event){
+    answerSelect(li4,3);
+});
 
 // quiz repository
-var quizlist = [
+var quizList = [
     {
         question: "What does CSS stand for?",
         options:["Colorful Style Sheets","Creative Style Sheets","Cascading Style Sheets","Computer Style Sheets"],
@@ -57,132 +169,3 @@ var quizlist = [
         rightAnswer:3
     },
 ]
-
-//  counter
-var i = 0;
-var c = 0;
-var w = 0;
-
-// Start the quiz at the click of the start button
-var startButton = document.querySelector("button");
-startButton.addEventListener("click",function(){
-    i = 0;
-    newQuiz();
-});
-
-// Display qiuz contents on the page
-// Create unordered list element
-var mainPageHeader = document.querySelector("h2");
-var mainContainer = document.getElementById("main_content");
-
-var list = document.createElement("ul");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
-
-li1.textContent = quizlist[i].options[0];
-li2.textContent = quizlist[i].options[1];
-li3.textContent = quizlist[i].options[2];
-li4.textContent = quizlist[i].options[3];
-
-// Show new quiz
-function newQuiz() {
-    mainPageHeader.textContent = quizlist[i].question;
-    li1.textContent = quizlist[i].options[0];
-    li2.textContent = quizlist[i].options[1];
-    li3.textContent = quizlist[i].options[2];
-    li4.textContent = quizlist[i].options[3];
-    mainContainer.appendChild(list);
-    list.appendChild(li1);
-    list.appendChild(li2);
-    list.appendChild(li3);
-    list.appendChild(li4);
-};
-
-// Add answer check and style change when one of the optisons are selected
-var bottomText = document.getElementById("bottom_content");
-
-li1.addEventListener("click",function(event){
-    if (quizlist[i].rightAnswer === 0) {
-        li1.setAttribute("class", "correct");
-        c++;
-        bottomText.textContent = "It's correct! You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
-    } else {
-        li1.setAttribute("class", "wrong");
-        w++;
-        bottomText.textContent = "It's wrong. You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
-    };
-    // Prevent from users to click again
-    list.setAttribute("style","pointer-events: none;");
-    // Wait for one second and show new quiz
-    setTimeout(function(){
-        i++;
-        newQuiz();
-        li1.setAttribute("class", "");
-        bottomText.textContent = "";
-        list.setAttribute("style","pointer-events: auto;");
-    },1000)
-});
-
-li2.addEventListener("click",function(){
-    if (quizlist[i].rightAnswer === 1) {
-        li2.setAttribute("class", "correct");
-        c++;
-        bottomText.textContent = "It's correct! You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
-    } else {
-        li2.setAttribute("class", "wrong");
-        w++;
-        bottomText.textContent = "It's wrong. You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
-    };
-    list.setAttribute("style","pointer-events: none;");
-    setTimeout(function(){
-        i++;
-        newQuiz();
-        li2.setAttribute("class", "");
-        bottomText.textContent = "";
-        list.setAttribute("style","pointer-events: auto;");
-    },1000)
-});
-
-li3.addEventListener("click",function(){
-    if (quizlist[i].rightAnswer === 2) {
-        li3.setAttribute("class", "correct");
-        c++;
-        bottomText.textContent = "It's correct! You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
-    } else {
-        li3.setAttribute("class", "wrong");
-        w++;
-        bottomText.textContent = "It's wrong. You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
-    };
-    list.setAttribute("style","pointer-events: none;");
-    setTimeout(function(){
-        i++;
-        newQuiz();
-        li3.setAttribute("class", "");
-        bottomText.textContent = "";
-        list.setAttribute("style","pointer-events: auto;");
-    },1000)
-});
-
-console.log(quizlist[i].rightAnswer);
-
-li4.addEventListener("click",function(){
-    if (quizlist[i].rightAnswer === 3) {
-        li4.setAttribute("class", "correct");
-        c++;
-        bottomText.textContent = "It's correct! You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
-    } else {
-        li4.setAttribute("class", "wrong");
-        w++;
-        bottomText.textContent = "It's wrong. You made " + c + " correct answer(s) and " + w + " wrong answer(s) so far."
-    };
-    list.setAttribute("style","pointer-events: none;");
-    setTimeout(function(){
-        i++;
-        newQuiz();
-        li4.setAttribute("class", "");
-        bottomText.textContent = "";
-        list.setAttribute("style","pointer-events: auto;");
-    },1000)
-});
